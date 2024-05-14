61f5c99b95bdf23a68e7ec575d5f2b21fa7ec9df
tauamcardozo <tauamcardozo@gmail.com>
2024-05-14 18:48:22 -0300

    Modified templates to display posts from database.

--- blog/models.py
+++ blog/models.py
@@ -5,18 +5,18 @@ from django.utils import timezone
 
 class Post(models.Model):
     
-    SEXO_CHOICES = (
-        ("F", "Feminino"),
-        ("M", "Masculino"),
-        ("N", "Nenhuma das opções")
-    )
+    #SEXO_CHOICES = (
+    #    ("F", "Feminino"),
+    #    ("M", "Masculino"),
+    #    ("N", "Nenhuma das opções")
+    #)
       
     author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     title = models.CharField(max_length=200)
     text = models.TextField()
     created_date = models.DateTimeField(default=timezone.now)
     published_date = models.DateTimeField(blank=True, null=True)
-    sexo = models.CharField(max_length=1, choices=SEXO_CHOICES, blank=False, null=False)
+    #sexo = models.CharField(max_length=1, choices=SEXO_CHOICES, blank=False, null=False)
 
     def publish(self):
         self.published_date = timezone.now()
